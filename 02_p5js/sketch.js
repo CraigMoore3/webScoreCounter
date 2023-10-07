c8d23a52fb0f952f75aa691056a7d41ca12a7692
Craig Moore <cdm21d@fsu.edu>
2023-10-07 12:00:37 -0400

    initialized plugin with p5

---
+++ 02_p5js/sketch.js
@@ -0,0 +1,11 @@
+console.log('sketch loaded');
+
+function setup() {
+    console.log('sketch setup');
+    createCanvas(400, 400);
+}
+
+function draw() {
+    console.log('sketch looping');
+    background(0);
+}
