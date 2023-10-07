8ab41d266432dcb35cdb391c735344b28edf7abb
Craig Moore <cdm21d@fsu.edu>
2023-10-07 11:35:34 -0400

    Imported repo contents

---
+++ 00_structure/sketch.js
@@ -0,0 +1,10 @@
+let width = 250, height = 250;
+
+function setup(){
+createCanvas(width, height);
+}
+
+function draw(){
+fill(125);
+rect(width/2, height/2, 50, 50);
+}
--- 01_eventTracking/content.js
+++ 01_eventTracking/content.js
@@ -1,1 +1,14 @@
 // --- Global Events --- //
+console.log("Chrome Extension is working");
+
+// --- Selects Element --- ///
+const pHover = document.querySelector('p');
+console.log(pHover);
+
+// --- Registers Event Listener --- ///
+pHover.addEventListener("click", shout);
+
+// --- Defines Event Function --- //
+function shout(event){
+    console.log("AHHH!");
+}
