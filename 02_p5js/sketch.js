582195f16b8c0648d1147bf8018404f19d3666df
Craig Moore <cdm21d@fsu.edu>
2023-10-07 12:29:05 -0400

    trouble with image linking

--- 02_p5js/sketch.js
+++ 02_p5js/sketch.js
@@ -1,11 +1,24 @@
 console.log('sketch loaded');
 
+let img1;
+
+// function preload() {
+    // img1 = loadImage('images/counterBackground01.png', img1 => {image(img1,0,0)});
+//     console.log('preloaded')
+// }
+
 function setup() {
     console.log('sketch setup');
-    createCanvas(400, 400);
+//     let canvas = createCanvas(400, 400);
+//     canvas.position(0,0);
+    img1 = loadImage('https://github.com/CraigMoore3/webScoreCounter/blob/main/02_p5js/images/counterBackground01.png', img1 => {image(img1,0,0)});
 }
 
 function draw() {
     console.log('sketch looping');
-    background(0);
 }
+
+// function scoreCount() {
+//     image(img1, 0, 0);
+//     console.log('scoreCount!');
+// }
