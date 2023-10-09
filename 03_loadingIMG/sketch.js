941f47077e318c7e62103fcb4c7e66eb546f2c72
Craig Moore <cdm21d@fsu.edu>
2023-10-09 10:15:47 -0400

    rob's help

--- 03_loadingIMG/sketch.js
+++ 03_loadingIMG/sketch.js
@@ -1,6 +1,11 @@
 console.log("version 0.3 Loaded");
 
 const scoreCounter = document.createElement("img");
-const url = chrome.runtime.getURL("counterBackground.png");
-    console.log(url);
-scoreCounter.src = url;
+const paragraph = document.createElement("p");
+paragraph.innerHTML = "Test";
+console.log(paragraph);
+// const url = chrome.runtime.getURL("counterBackground.png");
+    // console.log(url);
+scoreCounter.src = "https://static.rhizome.org/media/cache/e4/2a/e42a5e4f710e48d277f922ec5e5aeb5b.jpg";
+
+// https://static.rhizome.org/media/cache/e4/2a/e42a5e4f710e48d277f922ec5e5aeb5b.jpg
